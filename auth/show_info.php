9043c3e0300356adbf53d5b8b9d49e9910080c90
Artur <piterov.artur@outlook.com>
2022-02-20 12:33:57 +0300

    Исправил ошибку в show_info.php

--- auth/show_info.php
+++ auth/show_info.php
@@ -3,9 +3,8 @@ session_start();
 if (empty($_SESSION['login']) || empty($_SESSION['password'])) {
     die("Вы не вошли на сайт.");
 }
-$args = getopt("h:l:p:db");
-$db = mysqli_connect($args['h'], $args['l'], $args['p']);
-mysqli_select_db($db, $args['db']);
+$db = mysqli_connect($argv[2], $argv[3], $argv[4]);
+mysqli_select_db($db, $argv[5]);
 $result = mysqli_query($db, "SELECT * FROM users WHERE login='${_SESSION['login']}'");
 $myRow = mysqli_fetch_array($result);
 echo "Логин: ${myRow['login']}"
