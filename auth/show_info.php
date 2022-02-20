a29855755cb3fe6aadc88e52a631c12fc2bf9f05
Artur <piterov.artur@outlook.com>
2022-02-20 14:14:59 +0300

    Подправил аргументы

--- auth/show_info.php
+++ auth/show_info.php
@@ -3,8 +3,8 @@ session_start();
 if (empty($_SESSION['login']) || empty($_SESSION['password'])) {
     die("Вы не вошли на сайт.");
 }
-$db = mysqli_connect($argv[2], $argv[3], $argv[4]);
-mysqli_select_db($db, $argv[5]);
+$db = mysqli_connect("localhost", $argv[1], $argv[2]);
+mysqli_select_db($db, $argv[3]);
 $result = mysqli_query($db, "SELECT * FROM users WHERE login='${_SESSION['login']}'");
 $myRow = mysqli_fetch_array($result);
 echo "Логин: ${myRow['login']}"
